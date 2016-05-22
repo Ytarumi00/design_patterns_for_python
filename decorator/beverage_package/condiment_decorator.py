38895ec43517a6dc2605c6767b45f041173eee66
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-05-22 23:39:49 +0900

    add a concept size

--- decorator/beverage_package/condiment_decorator.py
+++ decorator/beverage_package/condiment_decorator.py
@@ -5,5 +5,11 @@ from abc import abstractmethod
 
 class CondimentDecorator(Beverage):
   @abstractmethod
+  def __init__(self, beverage):
+    Beverage.__init__(self)
+    self.beverage = beverage
   def getDescription(self):
     pass
+  
+  def getSize(self):
+    return self.beverage.getSize()
