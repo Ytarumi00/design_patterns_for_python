4dcac521b7d8c7562269bfb0202bf48448f1da96
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-07-06 01:55:01 +0900

    Test Singleton pattern
    Not applicable Singleton

---
+++ singleton/chocolate_boiler.py
@@ -0,0 +1,51 @@
+
+class ChocolateBoiler:
+  '''
+  singleton pattern test
+  
+  Attributes:
+    empty: A boolean indicating if boiler is empty.
+    boiled: A boolean indicating if boiler is boiled.
+  '''
+  
+  def __init__(self):
+    self.empty = True
+    self.boiled = False
+    
+  def fill(self):
+    if(self.isEmpty()):
+      print("牛乳とチョコの混合液を入れる")
+      self.empty = False
+      self.boiled = False
+    else:
+      print("#boilerが空でなければ混合液を入れられません")
+      
+  def drain(self):
+    if(not self.isEmpty() and self.isBoiled()):
+      print("沸騰した混合液を空にする")
+      self.empty = True
+    else:
+      print("#沸騰していない，混合液がからです")
+      
+  def boil(self):
+    if(not self.isEmpty() and not self.isBoiled()):
+      print("中身を沸騰させる")
+      self.boiled = True
+    else:
+      print("#沸騰しているか，混合液がからです")
+      
+  def isEmpty(self):
+    return self.empty
+  
+  def isBoiled(self):
+    return self.boiled
+      
+if __name__ == "__main__":
+  boiler1 = ChocolateBoiler()
+  boiler2 = ChocolateBoiler()
+  boiler1.fill()
+  boiler2.fill()
+  boiler1.boil()
+  boiler2.boil()
+  boiler1.drain()
+  boiler2.drain()
