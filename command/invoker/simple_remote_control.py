50a3a66852dfbaa8e671839dfdf8cbd07fb5ec39
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-07-29 03:05:20 +0900

    Add Simple Remote Control sets

---
+++ command/invoker/simple_remote_control.py
@@ -0,0 +1,13 @@
+from command.commnad_package.command import Command
+
+class SimpleRemoteControl:
+  
+  def __init__(self):
+    pass
+  
+  def setCommand(self, command :Command):
+    self.slot = command
+    
+  def buttonWasPressed(self):
+    self.slot.execute()
+    
---
+++ command/vender_class/light.py
@@ -0,0 +1,16 @@
+
+
+class Light:
+  '''
+  Vender class for Light
+  '''
+  
+  def __init__(self):
+    pass
+  
+  def on(self):
+    print("turn the light on")
+    
+  def off(self):
+    print("turn the light off")
+    
