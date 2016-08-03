342d33f1e235dea017c985e0f5238ebc70939f9e
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-08-04 00:15:45 +0900

    add garagedoor

---
+++ command/vender_class/garagedoor.py
@@ -0,0 +1,25 @@
+
+class GarageDoor:
+  '''
+  Vender class for GarageDoor
+  '''
+  
+  def __init__(self):
+    pass
+  
+  def up(self):
+    print("open garage")
+    
+  def down(self):
+    print("close garage")
+    
+  def stop(self):
+    print("stop garage door")
+    
+  def lightOn(self):
+    print("turn the light on")
+    
+  def lightOff(self):
+    print("turn the light off")
+    
+    
