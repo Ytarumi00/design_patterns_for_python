36045022994ad2d2823d8564b84d9b9c32f3d5c4
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-08-08 07:27:50 +0900

    add remote_loader and modify some modules

--- command/vender_class/garagedoor.py
+++ command/vender_class/garagedoor.py
@@ -4,22 +4,29 @@ class GarageDoor:
   Vender class for GarageDoor
   '''
   
-  def __init__(self):
-    pass
+  def __init__(self, *args):
+    self.name = ""
+    if len(args) != 0 :
+        self.name = args[0]
   
   def up(self):
+    print(self.name)
     print("open garage")
     
   def down(self):
+    print(self.name)
     print("close garage")
     
   def stop(self):
+    print(self.name)
     print("stop garage door")
     
   def lightOn(self):
+    print(self.name)
     print("turn the light on")
     
   def lightOff(self):
+    print(self.name)
     print("turn the light off")
     
     
