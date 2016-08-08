a1c234fc5426bfc77f590ba2bcfa8f95e563ad9e
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-08-09 03:40:29 +0900

    rename incorrect class name

--- command/command_package/ceiling_fan_low_command.py
+++ command/command_package/ceiling_fan_low_command.py
@@ -7,7 +7,7 @@ from command.command_package.command import Command
 from command.vender_class.ceiling_fan import CeilingFan
 
 
-class CeilingFanLowCommadn(Command):
+class CeilingFanLowCommand(Command):
     '''
     concrete command class for ceiling fan status changed high
     '''
