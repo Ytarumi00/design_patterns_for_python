2623fb0ecaaafd5bcc378e136a24f0b8793581cd
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-08-09 03:59:14 +0900

    refactoring

--- command/vender_class/ceiling_fan.py
+++ command/vender_class/ceiling_fan.py
@@ -10,29 +10,32 @@ class CeilingFan:
     Vender class for ceiling fan
     '''
 
-    __HIGH = 3
-    __MEDIUM = 2
-    __LOW = 1
-    __OFF = 0
+    HIGH = 3
+    MEDIUM = 2
+    LOW = 1
+    OFF = 0
 
     def __init__(self, location):
         '''
         Constructor
         '''
         self.location = location
-        self.speed = CeilingFan.__OFF
+        self.speed = CeilingFan.OFF
 
     def high(self):
-        self.speed = CeilingFan.__HIGH
+        self.speed = CeilingFan.HIGH
 
     def medium(self):
-        self.speed = CeilingFan.__MEDIUM
+        self.speed = CeilingFan.MEDIUM
 
     def low(self):
-        self.speed = CeilingFan.__LOW
+        self.speed = CeilingFan.LOW
 
     def off(self):
-        self.speed = CeilingFan.__OFF
+        self.speed = CeilingFan.OFF
 
     def getSpeed(self):
         return self.speed
+
+    def getStatus(self):
+        return "Fan's strength is {} ".format(self.getSpeed())
