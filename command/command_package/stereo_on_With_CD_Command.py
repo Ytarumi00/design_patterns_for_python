42a6ca228cbb4c8001d0da26cf2e42c09bfbc40c
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-08-10 08:40:02 +0900

    add macro command and loader

--- command/command_package/stereo_on_With_CD_Command.py
+++ command/command_package/stereo_on_With_CD_Command.py
@@ -6,14 +6,16 @@ Created on 2016/08/07
 from command.vender_class.stereo import Stereo
 from command.command_package.command import Command
 
+
 class StereoOnWithCDCommand(Command):
 
     def __init__(self, stereo: Stereo):
         self.stereo = stereo
-        
+
     def execute(self):
         self.stereo.on()
         self.stereo.setCD()
         self.stereo.setVolume(11)
-        
-        
+
+    def undo(self):
+        self.stereo.off()
