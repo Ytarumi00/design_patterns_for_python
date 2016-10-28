23017630f7f8a88a4c41e1a22a524c942ae46862
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-10-28 11:14:20 +0900

    Revert "rename class"
    This reverts commit 28816c7dedfaf14ed03b7a64b944c57e1dc8a384.

--- strategy/main/start.py
+++ strategy/main/start.py
@@ -1,6 +1,6 @@
-from strategy.duck_package.mallard_duck import MallardDuck
+from strategy.duck_package.mallard_duck import MaddardDuck
 
 if __name__ == "__main__":
-  my_duck = MallardDuck()
+  my_duck = MaddardDuck()
   my_duck.perform_fly()
   my_duck.perform_quack()
