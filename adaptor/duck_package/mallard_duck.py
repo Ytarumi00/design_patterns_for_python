8a77cfee6491c3959ef5c6ee5e0e10d6361a98ca
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-10-28 11:14:20 +0900

    Revert "rename class"
    This reverts commit 28816c7dedfaf14ed03b7a64b944c57e1dc8a384.

--- adaptor/duck_package/mallard_duck.py
+++ adaptor/duck_package/mallard_duck.py
@@ -2,14 +2,14 @@ from strategy.duck_package.duck import Duck
 from strategy.fly_package.fly_with_wings import FlyWithWings
 from strategy.quack_package.quack import Quack
 
-class MallardDuck(Duck):
+class MaddardDuck(Duck):
   def __init__(self):
     self.fly_behavior = FlyWithWings
     self.quack_behavior = Quack
     
 
 if __name__ == "__main__":
-  my_duck = MallardDuck()
+  my_duck = MaddardDuck()
   my_duck.perform_fly()
   my_duck.perform_quack()
   
