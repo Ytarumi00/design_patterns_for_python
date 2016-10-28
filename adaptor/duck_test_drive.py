4804fa1718272ffd801ce9ba617e2874f726135f
Y.Tarumi <y.tarumi00+dev@gmail.com>
2016-10-28 11:12:02 +0900

    completion main module for test

--- adaptor/duck_test_drive.py
+++ adaptor/duck_test_drive.py
@@ -1,8 +1,34 @@
 '''
 Created on 2016/08/13
 
-@author: yu-suke
+@author: Y.Tarumi
 '''
+from adaptor.duck_package.duck import Duck
+from adaptor.duck_package.mallard_duck import MallardDuck
+from adaptor.adaptor_package.turkey_adaptor import TurkeyAdaptor
+from adaptor.turkey_package.wild_turkey import WildTurkey
+
+__author__ = "Y.Tarumi<y.tarumi00+dev@gmail.com>"
+__version__ = "1.0.0"
+__date__ = "10/28/2016"
+
+def testDuck(duck :Duck):
+    duck.perform_quack()
+    duck.perform_fly()
+
 
 if __name__ == '__main__':
-    pass
+    duck = MallardDuck()
+
+    turkey = WildTurkey()
+    turkeyAdaptor = TurkeyAdaptor(turkey)
+
+    print("Turkeyの出力・・・")
+    turkey.gobble()
+    turkey.fly()
+
+    print("\nDuckの出力・・・")
+    testDuck(duck)
+
+    print("\nTurkeyAdapterの出力・・・")
+    testDuck(turkeyAdaptor)
