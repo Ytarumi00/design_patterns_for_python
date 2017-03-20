774f268169ec2c5fd5047c5fc8f68191feeb7050
Y.Tarumi <y.tarumi00+dev@gmail.com>
2017-03-20 16:33:44 +0900

    refactoring

--- state/has_quarter_state.py
+++ state/has_quarter_state.py
@@ -4,7 +4,7 @@ Created on 2017/03/18
 @author: yu-suke
 '''
 from state.abc_state import State
-from random import randrange
+from random import randrange, choice
 
 
 class HasQuarterState(State):
@@ -17,6 +17,8 @@ class HasQuarterState(State):
         Constructor
         '''
         super().__init__(gum_ball_machine)
+        self.seq = range(10)
+        self._set_is_winner()
 
     def insert_quarter(self):
         print("You cann't put in another 25 cents again")
@@ -28,12 +30,17 @@ class HasQuarterState(State):
 
     def turn_crank(self):
         print("Turned the crank")
-        if randrange(10) == 0:
+        if self.is_winner:
             self._gum_ball_machine.set_state(
                 self._gum_ball_machine.get_winner_state())
         else:
             self._gum_ball_machine.set_state(
                 self._gum_ball_machine.get_sold_state())
 
+        self._set_is_winner()
+
     def dispense(self):
         print("There is no gum ball to sell")
+
+    def _set_is_winner(self):
+        self.is_winner = True if choice(self.seq) == 0 else False
