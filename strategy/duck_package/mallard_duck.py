2e9d3820398dd0bf04b32feab14d902c012883bc
Y.Tarumi <y.tarumi00+dev@gmail.com>
2017-11-03 19:29:49 +0900

    rename dir

--- strategy/duck_package/mallard_duck.py
+++ strategy/duck_package/mallard_duck.py
@@ -2,15 +2,15 @@ from strategy.duck_package.duck import Duck
 from strategy.fly_package.fly_with_wings import FlyWithWings
 from strategy.quack_package.quack import Quack
 
+
 class MallardDuck(Duck):
-  def __init__(self):
-    self.fly_behavior = FlyWithWings
-    self.quack_behavior = Quack
-    
+
+    def __init__(self):
+        self.fly_behavior = FlyWithWings
+        self.quack_behavior = Quack
+
 
 if __name__ == "__main__":
-  my_duck = MallardDuck()
-  my_duck.perform_fly()
-  my_duck.perform_quack()
-  
-  
+    my_duck = MallardDuck()
+    my_duck.perform_fly()
+    my_duck.perform_quack()
